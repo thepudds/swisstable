c46de930d62e12e9eb8c7eab3a49b5f2bade4351
thepudds <thepudds@users.noreply.github.com>
2022-02-16 12:12:39 -0500

    mask: somewhat clean up avo code, reorder, add comments

--- avo/asm.go
+++ avo/asm.go
@@ -1,6 +1,3 @@
-// //go:build ignore
-// // +build ignore
-
 package main
 
 import (
@@ -48,41 +45,56 @@ import (
 func main() {
 	TEXT("MatchByte", NOSPLIT, "func(c uint8, buffer []byte) (mask uint32, ok bool)")
 	// TEXT("MatchByte", NOSPLIT, "func(c uint8, buffer *byte) uint32")
+	Comment("Get our input parameters")
+	c := Load(Param("c"), GP32())
+	ptr := Load(Param("buffer").Base(), GP64())
 	n := Load(Param("buffer").Len(), GP64())
 	result := GP32()
-	// ok := GP8()
+
+	Comment("Check len of our input slice, which must be at least 16")
 	CMPQ(n, operand.Imm(16))
 	JGE(operand.LabelRef("valid"))
 	// XORB(ok, ok)
 	// Store(ok, ReturnIndex(1))
+	Comment("Input slice too short. Return 0, false")
 	ok, err := ReturnIndex(1).Resolve()
 	if err != nil {
 		panic(err)
 	}
-	// TODO: return zero value ;=)
 	XORL(result, result)
 	Store(result, ReturnIndex(0))
 	MOVB(operand.Imm(0), ok.Addr)
 	RET()
 
 	Label("valid")
-	c := Load(Param("c"), GP32())
-	ptr := Load(Param("buffer").Base(), GP64())
+	Comment("Input slice is a valid length")
 
 	// ptr := Load(Param("buffer"), GP64())
+	Comment("Move c into an xmm register")
 	x0, x1, x2 := XMM(), XMM(), XMM()
-	PXOR(x1, x1)
 	MOVD(c, x0)
+	Comment("Shuffle the value of c into every byte of another xmm register")
+	PXOR(x1, x1)
 	PSHUFB(x1, x0)
 	// if !operand.IsM128(operand.Mem{Base: ptr}) {
 	// 	panic("not m128")
 	// }
 	// Mem example from https://github.com/mmcloughlin/avo/blob/master/examples/fnv1a/asm.go#L32
 	// also: https://github.com/mmcloughlin/avo/blob/master/examples/sum/asm.go
-	// MOVOU is how MOVDQU is spelled in Go asm
+	// https://c9x.me/x86/html/file_module_x86_id_184.html
+	//   When the source or destination operand is a memory operand, the operand may be unaligned on a 16-byte boundary
+	//   without causing a general-protection exception (#GP) to be generated.
+	Comment("Do an unaligned move of 16 bytes of input slice data to xmm register")
+	Comment("MOVOU is how MOVDQU is spelled in Go asm")
 	MOVOU(operand.Mem{Base: ptr}, x2)
+
+	Comment("Find matching bytes with result in xmm register")
 	PCMPEQB(x2, x0)
+
+	Comment("Collapse matching bytes result down to an integer bitmask")
 	PMOVMSKB(x0, result)
+
+	Comment("Return bitmask, true")
 	Store(result, ReturnIndex(0))
 	// MOVB(operand.Imm(1), ok)
 	// Store(ok, ReturnIndex(1))
--- match_amd64.s
+++ match_amd64.s
@@ -5,23 +5,41 @@
 // func MatchByte(c uint8, buffer []byte) (mask uint32, ok bool)
 // Requires: SSE2, SSSE3
 TEXT ·MatchByte(SB), NOSPLIT, $0-37
-	MOVQ buffer_len+16(FP), AX
-	CMPQ AX, $0x10
+	// Get our input parameters
+	MOVBLZX c+0(FP), AX
+	MOVQ    buffer_base+8(FP), CX
+	MOVQ    buffer_len+16(FP), DX
+
+	// Check len of our input slice, which must be at least 16
+	CMPQ DX, $0x10
 	JGE  valid
+
+	// Input slice too short. Return 0, false
 	XORL AX, AX
 	MOVL AX, mask+32(FP)
 	MOVB $0x00, ok+36(FP)
 	RET
 
 valid:
-	MOVBLZX  c+0(FP), AX
-	MOVQ     buffer_base+8(FP), CX
-	PXOR     X1, X1
-	MOVD     AX, X0
-	PSHUFB   X1, X0
-	MOVOU    (CX), X1
-	PCMPEQB  X1, X0
+	// Input slice is a valid length
+	// Move c into an xmm register
+	MOVD AX, X0
+
+	// Shuffle the value of c into every byte of another xmm register
+	PXOR   X1, X1
+	PSHUFB X1, X0
+
+	// Do an unaligned move of 16 bytes of input slice data to xmm register
+	// MOVOU is how MOVDQU is spelled in Go asm
+	MOVOU (CX), X1
+
+	// Find matching bytes with result in xmm register
+	PCMPEQB X1, X0
+
+	// Collapse matching bytes result down to an integer bitmask
 	PMOVMSKB X0, AX
-	MOVL     AX, mask+32(FP)
-	MOVB     $0x01, ok+36(FP)
+
+	// Return bitmask, true
+	MOVL AX, mask+32(FP)
+	MOVB $0x01, ok+36(FP)
 	RET
